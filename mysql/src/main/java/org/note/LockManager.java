57c8d7723aa63217a21e91732f661fe18bd19084
lixinxuan <lixinxuan@zirugroup.com>
2026-01-05 10:46:50 +0800

    feat：Distribute transaction and mysql lock

--- mysql/src/main/java/org/note/LockManager.java
+++ mysql/src/main/java/org/note/LockManager.java
@@ -6,7 +6,19 @@ package org.note;
 public interface LockManager {
 
     /**
-     * 按粒度：表锁（没命中索引时，给聚簇索引都加行锁） 行锁。
+     * 按粒度：
+     * <ul>
+     *     <li>表锁：未命中索引时，给聚簇索引加行锁和间隙锁（-oo, +oo）</li>
+     *     <li>行锁：锁定当前索引</li>
+     *     <li>
+     *         间隙锁：对已有索引范围加锁（非唯一索引。如果是唯一索引的等值查询只会加行锁），防止幻读。仅仅RR隔离级别下有效。
+     *         <p>eg：1 3 6 9
+     *         <p>select ... where column = 6 : 会锁住 6 这个索引和 (3,6) 这个间隙（默认锁左边，因为右边的索引还没扫描）。
+     *         <p>select ... where column = 7 : (6,9) 这个间隙。
+     *         <p>select ... where column = 10 : (9,+oo) 这个间隙。
+     *     </li>
+     *     <li>Next-Key Lock：行锁 + 间隙锁 </li>
+     * </ul>
      */
     void lock();
 }
--- mysql/src/main/java/org/note/TransactionManager.java
+++ mysql/src/main/java/org/note/TransactionManager.java
@@ -22,4 +22,67 @@ public interface TransactionManager {
      * </ul>
      */
     void transaction();
+
+    /**
+     * 分布式事务
+     *
+     * <ol>
+     * <li>
+     *     2PC（Two-Phase Commit）：二阶段提交。组件：TM：事务管理器， RM：资源管理器（DB）
+     *     <ol>
+     *         <li>PreCommit(预提交)，TM向RM发起prepare，RM会执行本地事务但是不提交（已生成undo.log和redo.log）</li>
+     *         <li>commit/rollback（决策阶段）：TM根据RM返回结果决定是提交还是回滚</li>
+     *     </ol>
+     *     缺点：阻塞
+     *     <ul>
+     *         <li>RM会加锁等TM决策，高并发环境下阻塞严重</li>
+     *         <li>TM单点故障，RM会长时间阻塞</li>
+     *         <li>二阶段决策，有RM故障时（网络不可靠等）会产生不一致，需要人工解决</li>
+     *     </ul>
+     * </li>
+     * <li>
+     *     3PC（Three-Phase Commit）：三阶段提交，组件：和2pc一样。
+     *     <p>解决了2pc的阻塞问题，RM可以超时提交。
+     *     <ol>
+     *         <li>CanCommit（询问阶段），TM问所有RM是否可以执行事务，RM不会做真的提交，只会做校验相关，判断这个事务是否能成功执行</li>
+     *         <li>PreCommit(预提交)，和2pc一样，但是会加个超时自动提交的定时器</li>
+     *         <li>commit/rollback（决策阶段）：TM根据RM返回结果决定是提交还是回滚</li>
+     *     </ol>
+     *     缺点：复杂度高，性能不理想。
+     * </li>
+     * <br/>
+     * <strong>
+     *     <p>⬆⬆⬆前面都是强一致的方案⬆⬆⬆
+     *     <p>⬇⬇⬇后面的方案从根本上做出了改变，不再要求强一致，只需要保证最终一致就行⬇⬇⬇
+     * </strong>
+     * <br/>
+     * <li>
+     *     TCC（Try-Confirm-Cancel）：业务层自己实现，一般需要定义并实现3个接口 prepare（Try 冻结），commit（Confirm 提交），rollback（Cancel 回滚）。
+     *     <p>解决了阻塞问题（无锁）。
+     *     缺点：业务侵入，每个地方都需要手动实现。
+     * </li>
+     * <li>
+     *     Saga：为每一步增加一个补偿，当失败时执行前面已执行的补偿事务。
+     *     <p>解决了阻塞问题（无锁）。
+     *     缺点：一致性弱，每个业务都需要设计补偿事务。
+     * </li>
+     * <li>
+     *     最大努力通知：事务发起方本地事务执行成功后，主动通知（重试） + 结果查询接口 通知第三方保证最终一致。
+     * </li>
+     * <li>
+     *     事务消息：先发送half消息到broker，本地事务执行完提交后告诉broker提交，broker将消息投递给消费者（未确认前消息不可见），
+     *     如果broker在规定时间内没接受到生产者的确认消息，则会进行回查（生产者检查本地事务的执行结果返回给broker）。
+     * </li>
+     * <li>
+     *     Seata：基于AT（Automatic Transaction）模式（默认都是该模式）的分布式事务。还支持其它模式，eg：XA（数据库层面的规范，基于2pc实现），SAGA，TCC等。
+     *     <ul>
+     *         组件：
+     *         <li>TM（事务管理器）：@GlobalTransactional 标记的服务都可以视为TM，向TC注册事务，发起提交/回滚。</li>
+     *         <li>RM（资源管理器）：引入seata后被动感知自己是分支事务（根据组件内全局XID实现），执行完后会向TC注册自己的状态。</li>
+     *         <li>TC（事务协调器）：通知RM提交/回滚，同时他也是兜底组件，因为RM注册到了TC，即使TM挂了，他也能代替TM完成事务。</li>
+     *     </ul>
+     * </li>
+     * </ol>
+     */
+    void distributeTransaction();
 }
